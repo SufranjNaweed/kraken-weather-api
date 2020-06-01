21860c1bd8a4a8bd3cc5f5a5a3451724f271cce8
SufranjNaweed <sufranj.naweed@gmail.com>
2020-06-02 01:01:16 +0200

    add : get weather routes

--- services/weathers/index.js
+++ services/weathers/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const axios = require('axios');
 
-// @route   GET api/weathers
-// @desc    Get weathers
+const api_url = process.env.API_URL;
+const api_key = process.env.API_KEY;
+
+// @route   POST api/weathers
+// @desc    POST weathers
 // @access  Public
-router.get('/', async(req, res) => {
+router.post('/', async(req, res) => {
     try{
         console.log("api routes  api/weathers");
     }
@@ -12,6 +17,39 @@ router.get('/', async(req, res) => {
         console.error(err.message);
         res.status(500).send('Server ERROR');
     }
+});
+
+//  @route  GET api/weathers
+//  @desc   GET weathers for a given city
+//  @access Public
+
+router.get('/', 
+    [
+        check('city', 'Please include a valide city').exists()
+    ],
+    async(req, res) => {
+        console.log('get city')
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res
+                    .status(400)
+                    .json({errors : errors.array()});
+        }
+        try{
+            const { city } = req.body;
+            const url = 'https://' + api_url + 'weather' + '?q=' + city + '&units=metric' + '&appid=' + api_key;
+            const response = await axios.get(url);
+            return res.status(200).send(response.data);Z
+        }
+        catch(err){
+            console.log(err.response.status);
+            if (err.response.status == 404){
+                res.status(404).send('City not found.');
+            }
+            else{
+                res.status(500).send('Server Error');
+            }
+        }
 });
 
 module.exports  = router;
