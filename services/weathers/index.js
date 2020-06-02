a88488e60ff6fb7ea0671222c6b13d547528647d
SufranjNaweed <sufranj.naweed@gmail.com>
2020-06-02 16:20:21 +0200

    add : forecast route & update : url api

--- services/weathers/index.js
+++ services/weathers/index.js
@@ -37,7 +37,8 @@ router.get('/',
         }
         try{
             const { city } = req.body;
-            const url = 'https://' + api_url + 'weather' + '?q=' + city + '&units=metric' + '&appid=' + api_key;
+            const url = api_url + 'current.json?key=' + api_key + "&q=" + city;
+            console.log(url);
             const response = await axios.get(url);
             return res.status(200).send(response.data);Z
         }
@@ -51,5 +52,35 @@ router.get('/',
             }
         }
 });
+
+router.get('/forecast',
+    [
+        check('city', 'Please include a valide city').exists(),
+        check('days', 'Please include a number of days').exists()
+    ],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res
+                    .status(400)
+                    .json({errors : errors.array()});
+        }
+        try{
+            const { city, days } = req.body;
+            const url = api_url + "forecast.json?key=" + api_key + "&q=" + city + "&days=" + days;
+            const response = await axios.get(url);
+            return res.status(200).send(response.data);Z
+        }
+        catch(err){
+            console.log(err);
+            if (err.response.status == 404){
+                res.status(404).send('City not found.');
+            }
+            else{
+                res.status(500).send('Server Error');
+            }
+        }
+    }
+)
 
 module.exports  = router;
